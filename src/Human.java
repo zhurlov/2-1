6c3a1dbc0aaf95e87328f4e97f0a0901e8e31948
Станислав <zurlov.s.s@outlook.com>
2022-11-16 22:55:05 +0300

    homeWork 2-1. л.

--- src/Human.java
+++ src/Human.java
@@ -4,10 +4,20 @@ public class Human {
     public int yearOfBirth;
     public String town;
 
+    Human(String name, String jobTitle, int yearOfBirth, String town) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.yearOfBirth = yearOfBirth;
+        this.town = town;
+    }
+
+
     void men () {
         System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + " .Будем знакомы!");
     }
     void girls () {
         System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родилась в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + " .Будем знакомы!");
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,34 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human maksim = new Human();
-        maksim.name = "Максим";
-        maksim.yearOfBirth = 1992;
-        maksim.town = "Екатеринбург";
-        maksim.jobTitle = "программист";
+        Human maksim = new Human("Максим", "программист",1992, "Екатеринбург");
+        //maksim.name = "Максим";
+        //maksim.yearOfBirth = 1992;
+        //maksim.town = "Екатеринбург";
+        //maksim.jobTitle = "программист";
         maksim.men();
 
-        Human anya = new Human();
-        anya.name = "Аня";
-        anya.yearOfBirth = 1985;
-        anya.town = "Москва";
-        anya.jobTitle = "гейм-дизайнер";
+        Human anya = new Human("Аня", "гейм-дизайнер", 1985, "Москва");
+        //anya.name = "Аня";
+        //anya.yearOfBirth = 1985;
+        //anya.town = "Москва";
+        //anya.jobTitle = "гейм-дизайнер";
         anya.girls();
 
-        Human katia = new Human();
-        katia.name = "Катя";
-        katia.yearOfBirth = 2000;
-        katia.town = "Сочи";
-        katia.jobTitle = "судебный секретарь";
+        Human katia = new Human("Катя", "судебный секретарь", 2000, "Сочи");
+        //katia.name = "Катя";
+        //katia.yearOfBirth = 2000;
+        //katia.town = "Сочи";
+        //katia.jobTitle = "судебный секретарь";
         katia.girls();
 
-        Human artyom = new Human();
-        artyom.name = "Артем";
-        artyom.yearOfBirth = 1974;
-        artyom.town = "Череповец";
-        artyom.jobTitle = "оператор вышки";
+        Human artyom = new Human("Артем", "оператор вышки", 1974, "Череповец");
+        //artyom.name = "Артем";
+        //artyom.yearOfBirth = 1974;
+        //artyom.town = "Череповец";
+        //artyom.jobTitle = "оператор вышки";
         artyom.men();
+
 
     }
 }
