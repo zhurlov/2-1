e967c01ffccda5287b7f9218fd82d7aa8172deaa
Станислав <zurlov.s.s@outlook.com>
2022-11-16 10:25:01 +0300

    homeWork 2-1. Люди.

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+
+    public String name, jobTitle;
+    public int yearOfBirth;
+    public String town;
+
+    void men () {
+        System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + " .Будем знакомы!");
+    }
+    void girls () {
+        System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родилась в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + " .Будем знакомы!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
+        Human maksim = new Human();
+        maksim.name = "Максим";
+        maksim.yearOfBirth = 1992;
+        maksim.town = "Екатеринбург";
+        maksim.jobTitle = "программист";
+        maksim.men();
+
+        Human anya = new Human();
+        anya.name = "Аня";
+        anya.yearOfBirth = 1985;
+        anya.town = "Москва";
+        anya.jobTitle = "гейм-дизайнер";
+        anya.girls();
+
+        Human katia = new Human();
+        katia.name = "Катя";
+        katia.yearOfBirth = 2000;
+        katia.town = "Сочи";
+        katia.jobTitle = "судебный секретарь";
+        katia.girls();
+
+        Human artyom = new Human();
+        artyom.name = "Артем";
+        artyom.yearOfBirth = 1974;
+        artyom.town = "Череповец";
+        artyom.jobTitle = "оператор вышки";
+        artyom.men();
+
     }
 }
