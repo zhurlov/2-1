e44a674f117b36ef88cb18be7f6404cd5c505f1a
Станислав <zurlov.s.s@outlook.com>
2022-11-17 12:56:46 +0300

    homeWork 2-1. Автомобили. д.з Выполнено.

--- src/Cars.java
+++ src/Cars.java
@@ -4,7 +4,34 @@ public class Cars {
     public int year;
     double engineVolume;
 
-    void carModels () {
+    Cars (String brand, String model, String color, String country, int year, double engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        if (color == null) {
+            this.color = "белый";
+        }
+        this.country = country;
+        this.year = year;
+        if (year <= 0) {
+            this.year = 2000;
+        }
+        this.engineVolume = engineVolume;
+        if (engineVolume <= 0) {
+            this.engineVolume = engineVolume + 1.5;
+        }
+        if (model == null) {
+            this.model = "default";
+        }
+        if (brand == null) {
+                this.brand = "default";
+        }
+        if (country == null) {
+                this.country = "default";
+        }
+
+        }
+        void carModels () {
         System.out.println(brand + " " + model + ", " + year + " года выпуска," + " сборка " + country + ", " + color + " цвет," + " объем двигателя " + engineVolume + " л.");
     }
 }
